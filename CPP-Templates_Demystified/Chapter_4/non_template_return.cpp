553acc048aad7065431eb869b0dad65846917d94
Vivek Bhadra <vivek.bhadra@gmail.com>
2025-11-20 23:55:14 +0000

    Update non_template_return.cpp

--- CPP-Templates_Demystified/Chapter_4/non_template_return.cpp
+++ CPP-Templates_Demystified/Chapter_4/non_template_return.cpp
@@ -9,17 +9,18 @@
 \*************************************************************************/
 
 #include <iostream>
+
 template<typename T>
-int is_equal(T x, T y)
+bool is_equal(T x, T y)
 {
     return x == y;
 }
 
 int main()
 {
-    int var1 = 20, var2 = 20;
-    std::cout << " var1 and var2 " 
-              << (is_equal(var1, var2) ? " are " : " are not ") 
-              << " equal" << std::endl;
+    int var1 = 10, var2 = 20;
+    std::cout << "var1 and var2 "
+              << (is_equal(var1, var2) ? "are " : "are not ")
+              << "equal\n";
     return 0;
 }
