30ea0d2b80aa538f92c43305115f9ed8bf2da4f0
Vivek Bhadra <vivek.bhadra@gmail.com>
2025-11-21 07:43:07 +0000

    Update explicit_return_type.cpp

--- CPP-Templates_Demystified/Chapter_4/explicit_return_type.cpp
+++ CPP-Templates_Demystified/Chapter_4/explicit_return_type.cpp
@@ -8,21 +8,25 @@
 * the file COPYING.gpl-v3 for details.                                    *
 \*************************************************************************/
 
+// return_type_correct.cpp
 #include <iostream>
+
 template<typename T, typename U>
-U largest_elem(const T number_set, size_t len)
+U largest_elem(const T data_set, size_t count)
 {
-    U largest = number_set[0];
-    for(size_t index = 0; index < len; ++index)
-        if (number_set[index] > largest)
-            largest = number_set[index];
+    U largest = data_set[0];
+    for (size_t i = 0; i < count; ++i)
+        if (data_set[i] > largest)
+            largest = data_set[i];
     return largest;
 }
 
 int main()
 {
     double numbers[] = {20.1, 10.1, 30.1, 5.1, 40.1, 200.1, 50.1, 1.1, 0.5, 100.5};
-    double l = largest_elem<double [], double>(numbers, 10);
-    std::cout << "largest = " << l << std::endl;
+
+    double result = largest_elem<double[], double>(numbers, 10);
+
+    std::cout << "largest = " << result << "\n";
     return 0;
 }
