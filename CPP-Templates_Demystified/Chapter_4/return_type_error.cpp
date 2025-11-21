a7c5200296a592c9ff26b90a20be44afd30558bb
Vivek Bhadra <vivek.bhadra@gmail.com>
2025-11-21 07:15:20 +0000

    Update return_type_error.cpp

--- CPP-Templates_Demystified/Chapter_4/return_type_error.cpp
+++ CPP-Templates_Demystified/Chapter_4/return_type_error.cpp
@@ -11,21 +11,24 @@
 #include <iostream>
 
 template<typename T>
-T largest_elem(const T number_set, size_t len)
+T largest_elem(const T data_set, size_t count)
 {
-    T largest = number_set[0];
-    for(size_t index = 0; index < len; ++index)
-        if (number_set[index] > largest)
-            largest = number_set[index];
-    return largest;
+    T max_value = data_set[0];
+    for (size_t i = 0; i < count; ++i)
+        if (data_set[i] > max_value)
+            max_value = data_set[i];
+    return max_value;
 }
 
 int main()
 {
-    double numbers[] = {20.1, 10.1, 30.1, 5.1, 40.1, 200.1, 50.1, 1.1,
-                        0.5, 100.5};
+    double numbers[] = {
+        20.1, 10.1, 30.1, 5.1, 40.1,
+        200.1, 50.1, 1.1, 0.5, 100.5
+    };
 
-    double l = largest_elem(numbers, 10);
-    std::cout << "largest = " << l << "\n";
+    double largest = largest_elem(numbers, 10);
+    std::cout << "largest = " << largest << "\n";
+
     return 0;
 }
